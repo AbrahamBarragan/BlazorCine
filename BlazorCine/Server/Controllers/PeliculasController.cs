3ab025540b5dde2150b3b16d68db115f3e808139
Abraham Barragan Lira <abraham.barragan02@gmail.com>
2022-12-07 15:44:36 -0700

    Agregar parametro

---
+++ BlazorCine/Server/Controllers/PeliculasController.cs
@@ -0,0 +1,24 @@
+﻿using BlazorCine.Server.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorCine.Server.Controllers
+{
+    [ApiController, Route("[controller]")]
+    public class PeliculasController : ControllerBase
+    {
+        private readonly ApplicationDbContext context;
+        public PeliculasController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Pelicula>>> Get()
+        {
+            return await context.Peliculas.ToListAsync();
+        }
+
+
+    }
+}
---
+++ BlazorCine/Shared/DTOs/PeliculaDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorCine.Shared.DTOs
+{
+	public class PeliculaDTO
+	{
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
